53f329a5db4cec0e51d3dbb8b3c1de51170bae9e
Амина Закирова <zakirova200272@gmail.com>
2021-11-20 10:48:28 +0400

    lab2

--- Zakirova advanced/FormTruck.cs
+++ Zakirova advanced/FormTruck.cs
@@ -11,46 +11,44 @@ using System.Windows.Forms;
 namespace Zakirova
 {
 	public partial class FormTruck : Form
-    {
-        
-        private DumpTruck truck;
+    {      
+        private ITTruck truck;
 
         public FormTruck()
 		{
-			InitializeComponent();		
-		}
+			InitializeComponent();
+        }
+
         /// <summary>
         /// Метод отрисовки машины
-        /// </summary>
-        
+        /// </summary>        
         private void Draw()
         {
             Bitmap bmp = new Bitmap(pictureBoxTruck.Width, pictureBoxTruck.Height);
             Graphics gr = Graphics.FromImage(bmp);
             truck.DrawTransport(gr);
-            pictureBoxTruck.Image = bmp;
-           
+            pictureBoxTruck.Image = bmp;           
         }
      
        
         /// <summary>
         /// Обработка нажатия кнопки "Создать"
         /// </summary>
-
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
             Random rnd = new Random();
-            truck = new DumpTruck();
             bool tmp = true;
-            if(Convert.ToInt32(trWheel.SelectedItem) == 2 || Convert.ToInt32(trWheel.SelectedItem) == 3 ||
-                Convert.ToInt32(trWheel.SelectedItem) == 4) { tmp = false; }
-            truck.Init(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Blue,
-            Color.Yellow, true, true, true, true, Convert.ToInt32(trWheel.SelectedItem), tmp);
-            truck.SetPosition(rnd.Next(80, 500), rnd.Next(10, 100), pictureBoxTruck.Width, pictureBoxTruck.Height);
+            if (Convert.ToInt32(trWheel.SelectedItem) == 2 || Convert.ToInt32(trWheel.SelectedItem) == 3 ||
+              Convert.ToInt32(trWheel.SelectedItem) == 4) { tmp = false; }
+            truck = new DumpTruck(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Blue,
+           Color.Yellow, true, true, true, true, Convert.ToInt32(trWheel.SelectedItem), tmp, ornwheels.SelectedIndex);            
+            truck.SetPosition(rnd.Next(80, 500),
+           rnd.Next(10, 100), pictureBoxTruck.Width, pictureBoxTruck.Height);
             Draw();
-        }
+        }      
+
         /// <summary>
         /// Обработка нажатия кнопок управления
         /// </summary>
@@ -58,7 +56,6 @@ namespace Zakirova
         /// <param name="e"></param>
         private void buttonMove_Click(object sender, EventArgs e)
         {
-            //получаем имя кнопки
             string name = (sender as Button).Name;
             switch (name)
             {
@@ -76,6 +73,6 @@ namespace Zakirova
                     break;
             }
             Draw();
-        }      
+        }       
     }
 }
---
+++ Zakirova advanced/Ornament1class.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Zakirova
+{
+    public class Ornament1class : InterDop
+    {
+        private EnumW newEnum;
+
+        public int Number { set => newEnum = (EnumW)value; }
+
+        public Ornament1class(int wh_cnt)
+        {
+            Number = wh_cnt;
+        }
+
+        public void DrawDop(Graphics g, float _startPosX, float _startPosY)
+        {
+            Pen dopwheelspen = new Pen(Color.Red);
+            Brush dopwheelsbrush = new SolidBrush(Color.Pink);
+            
+            g.DrawEllipse(dopwheelspen, _startPosX + 20, _startPosY + 24, 20, 20);
+            g.DrawEllipse(dopwheelspen, _startPosX + 75, _startPosY + 24, 20, 20);
+
+            g.FillEllipse(dopwheelsbrush, _startPosX + 20, _startPosY + 24, 20, 20);
+            g.DrawLine(dopwheelspen, _startPosX + 24, _startPosY + 25, _startPosX + 36, _startPosY + 43);
+            g.DrawLine(dopwheelspen, _startPosX + 36, _startPosY + 25, _startPosX + 24, _startPosY + 43);
+
+            g.FillEllipse(dopwheelsbrush, _startPosX + 75, _startPosY + 24, 20, 20);
+            g.DrawLine(dopwheelspen, _startPosX + 79, _startPosY + 25, _startPosX + 91, _startPosY + 43);
+            g.DrawLine(dopwheelspen, _startPosX + 91, _startPosY + 25, _startPosX + 79, _startPosY + 43);                               
+            
+            if (newEnum == EnumW.three)
+            {
+                g.DrawEllipse(dopwheelspen, _startPosX + 40, _startPosY + 24, 20, 20);
+
+                g.FillEllipse(dopwheelsbrush, _startPosX + 40, _startPosY + 24, 20, 20);
+                g.DrawLine(dopwheelspen, _startPosX + 44, _startPosY + 25, _startPosX + 56, _startPosY + 43);
+                g.DrawLine(dopwheelspen, _startPosX + 56, _startPosY + 25, _startPosX + 44, _startPosY + 43);
+            }
+            if (newEnum == EnumW.four)
+            {                
+                g.DrawEllipse(dopwheelspen, _startPosX + 40, _startPosY + 24, 20, 20);
+                g.DrawEllipse(dopwheelspen, _startPosX - 5, _startPosY + 24, 20, 20);                
+
+                g.FillEllipse(dopwheelsbrush, _startPosX + 40, _startPosY + 24, 20, 20);
+                g.DrawLine(dopwheelspen, _startPosX + 44, _startPosY + 25, _startPosX + 56, _startPosY + 43);
+                g.DrawLine(dopwheelspen, _startPosX + 56, _startPosY + 25, _startPosX + 44, _startPosY + 43);
+                
+                g.FillEllipse(dopwheelsbrush, _startPosX - 5, _startPosY + 24, 20, 20);
+                g.DrawLine(dopwheelspen, _startPosX - 1, _startPosY + 25, _startPosX + 11, _startPosY + 43);
+                g.DrawLine(dopwheelspen, _startPosX + 11, _startPosY + 25, _startPosX - 1, _startPosY + 43);
+            }
+        }
+
+    }
+}
